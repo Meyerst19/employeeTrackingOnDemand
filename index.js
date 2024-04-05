f60148bd8c6c3e012cdb36c7879050b70fbd1219
TME <tyler.s.meyers@gmail.com>
2024-04-05 12:41:27 -0600

    updating file structure to have inquirer routes built off a Class

--- index.js
+++ index.js
@@ -19,66 +19,66 @@ inquirer
       message:
         "Welcome to your OnDemand Employee Tracking Database. How may we best assit you today?",
       name: "welcomeMessage",
-      choices: [
-        "View Current",
-        "Add New",
-        "Update Existing",
-        "Delete a Record",
-        "Exit",
-      ],
-    },
-  ])
-  .then((response) => {
-    switch (response.welcomeMessage) {
-      case "View Current":
-        inquirer
-          .prompt([
-            {
-              type: "list",
-              message: "View Current Records for...",
-              name: "viewWhat",
-              choices: ["Departments", "Roles", "Employees"],
-            },
-          ])
-          .then();
-        break;
-      case "Add New":
-        inquirer
-          .prompt([
-            {
-              type: "list",
-              message: "Add New Record for...",
-              name: "addWhat",
-              choices: ["Departments", "Roles", "Employees"],
-            },
-          ])
-          .then();
-        break;
-      case "Update Existing":
-        inquirer
-          .prompt([
-            {
-              type: "list",
-              message: "Update existing data from...",
-              name: "updateWhat",
-              choices: ["Departments", "Roles", "Employees"],
-            },
-          ])
-          .then();
-        break;
-      case "Delete a Record":
-        inquirer
-          .prompt([
-            {
-              type: "list",
-              message: "Delete a record from...",
-              name: "deleteWhat",
-              choices: ["Departments", "Roles", "Employees"],
-            },
-          ])
-          .then();
-        break;
-      case "Exit":
-        return;
-    }
-  });
+      choices: [ "View all departments", "View all roles", "View all employees", "Add a department", "Add a role", "Add an employee", "Update employee role", "Update employee manager", "View employees by manager", "View employees by department", "Delete a department", "Delete a role", "Delete an employee", "View Utilized Budget" ]
+  //       "View Current",
+  //       "Add New",
+  //       "Update Existing",
+  //       "Delete a Record",
+  //       "Exit",
+  //     ],
+  //   },
+  // ])
+  // .then((response) => {
+  //   switch (response.welcomeMessage) {
+  //     case "View Current":
+  //       inquirer
+  //         .prompt([
+  //           {
+  //             type: "list",
+  //             message: "View Current Records for...",
+  //             name: "viewWhat",
+  //             choices: ["Departments", "Roles", "Employees"],
+  //           },
+  //         ])
+  //         .then();
+  //       break;
+  //     case "Add New":
+  //       inquirer
+  //         .prompt([
+  //           {
+  //             type: "list",
+  //             message: "Add New Record for...",
+  //             name: "addWhat",
+  //             choices: ["Departments", "Roles", "Employees"],
+  //           },
+  //         ])
+  //         .then();
+  //       break;
+  //     case "Update Existing":
+  //       inquirer
+  //         .prompt([
+  //           {
+  //             type: "list",
+  //             message: "Update existing data from...",
+  //             name: "updateWhat",
+  //             choices: ["Departments", "Roles", "Employees"],
+  //           },
+  //         ])
+  //         .then();
+  //       break;
+  //     case "Delete a Record":
+  //       inquirer
+  //         .prompt([
+  //           {
+  //             type: "list",
+  //             message: "Delete a record from...",
+  //             name: "deleteWhat",
+  //             choices: ["Departments", "Roles", "Employees"],
+  //           },
+  //         ])
+  //         .then();
+  //       break;
+  //     case "Exit":
+  //       return;
+  //   }
+  // });
